da01da3b2c58f8125e5b37891dd8907ac07f86da
durashere <walendes@gmail.com>
2021-01-22 21:44:52 +0100

    feat: added first navbar

--- src/components/modules/Header.js
+++ src/components/modules/Header.js
@@ -1,8 +1,9 @@
 import Link from 'next/link';
+import {firebaseClient} from '../../../firebaseClient';
 
-const NavItem = ({children}) => {
+const NavItem = ({href, children}) => {
   return (
-    <Link href={`/${children.toLowerCase()}`}>
+    <Link href={href}>
       <button className="text-3xl font-medium text-gray-900 outline-none">{children}</button>
     </Link>
   );
@@ -10,12 +11,20 @@ const NavItem = ({children}) => {
 
 const Header = () => {
   return (
-    <header className="mb-4">
+    <header className="flex justify-between mb-4">
       <nav className="flex items-end gap-4">
-        <NavItem>Dashboard</NavItem>
-        <NavItem>Templates</NavItem>
-        <NavItem>Toners</NavItem>
+        <NavItem href="/">Dashboard</NavItem>
+        <NavItem href="/templates">Templates</NavItem>
+        <NavItem href="/toners">Toners</NavItem>
       </nav>
+      <button
+        onClick={async () => {
+          await firebaseClient.auth().signOut();
+          window.location.href = '/';
+        }}
+      >
+        Logout
+      </button>
     </header>
   );
 };
