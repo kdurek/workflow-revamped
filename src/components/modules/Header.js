b2e462c5f61dff7af412ac70e215ccb25a76d617
durashere <walendes@gmail.com>
2021-01-22 11:25:11 +0100

    feat: added first nav

--- src/components/modules/Header.js
+++ src/components/modules/Header.js
@@ -1,7 +1,21 @@
+import Link from 'next/link';
+
+const NavItem = ({children}) => {
+  return (
+    <Link href={`/${children.toLowerCase()}`}>
+      <button className="text-3xl font-medium text-gray-900 outline-none">{children}</button>
+    </Link>
+  );
+};
+
 const Header = () => {
   return (
-    <header>
-      <p>Header</p>
+    <header className="mb-4">
+      <nav className="flex items-end gap-4">
+        <NavItem>Dashboard</NavItem>
+        <NavItem>Templates</NavItem>
+        <NavItem>Toners</NavItem>
+      </nav>
     </header>
   );
 };
