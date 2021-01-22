67c426bf263189cb62407d8868930b8c35bbdb80
durashere <walendes@gmail.com>
2021-01-22 21:42:30 +0100

    feat: added firebase authentication system

--- firebaseClient.js
+++ firebaseClient.js
@@ -1,35 +1,3 @@
-import firebase from 'firebase/app';
-// import 'firebase/firestore';
-// import 'firebase/storage';
-// import 'firebase/auth';
-
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  // databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-};
-
-if (typeof window !== 'undefined' && !firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-  // firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
-}
-
-// const auth = firebase.auth();
-// const db = firebase.firestore();
-const now = firebase.firestore.Timestamp.now();
-// const storage = firebase.storage();
-
-export {
-  // auth,
-  // db,
-  now,
-  // storage,
-};
-
 import firebaseClient from 'firebase/app';
 // import 'firebase/firestore';
 // import 'firebase/storage';
@@ -40,7 +8,7 @@ if (typeof window !== 'undefined' && !firebaseClient.apps.length) {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    // storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
     // databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,14 @@
 import PropTypes from 'prop-types';
-
 import '../styles/tailwind.css';
 
+import {AuthProvider} from '../context/auth';
+
 const CustomApp = ({Component, pageProps}) => {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 };
 
 CustomApp.propTypes = {
