71b68dc44e31ce7b2a388ce0cb8058e6f2ba3f47
durashere <walendes@gmail.com>
2021-01-22 21:46:20 +0100

    feat: added login verification
    when entering page it checks if user is logged in then redirect to login if not

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
+import nookies from 'nookies';
 import DefaultLayout from '../components/layouts/DefaultLayout';
 import Dashboard from '../components/templates/Dashboard';
+import {firebaseAdmin} from '../../firebaseAdmin';
 
 const Index = () => {
   return (
@@ -11,6 +13,37 @@ const Index = () => {
       <Dashboard />
     </DefaultLayout>
   );
+};
+
+export const getServerSideProps = async ctx => {
+  try {
+    const cookies = nookies.get(ctx);
+    // console.log(JSON.stringify(cookies, null, 2));
+    const token = await firebaseAdmin.auth().verifyIdToken(cookies.token);
+    const {uid, email} = token;
+
+    // console.log('token:', token);
+
+    return {
+      props: {email, uid},
+    };
+  } catch (err) {
+    // either the `token` cookie didn't exist
+    // or token verification failed
+    // either way: redirect to the login page
+    // either the `token` cookie didn't exist
+    // or token verification failed
+    // either way: redirect to the login page
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/login',
+      },
+      // `as never` is required for correct type inference
+      // by InferGetServerSidePropsType below
+      props: {},
+    };
+  }
 };
 
 export default Index;
