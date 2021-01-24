acac3ca0e58236532fdf5f2887f729e2d3ba2597
Krzysztof Durek <walendes@gmail.com>
2021-01-24 23:02:22 +0100

    style: Updated AuthLayout to follow theme

--- src/components/layouts/AuthLayout.js
+++ src/components/layouts/AuthLayout.js
@@ -4,8 +4,8 @@ import Footer from '../modules/Footer';
 
 const AuthLayout = ({children}) => {
   return (
-    <div className="grid h-screen mx-auto bg-gray-800 place-content-center">
-      <div className="container">
+    <div className="grid h-screen bg-coolGray-100 place-content-center">
+      <div className="container p-4 bg-white rounded-xl">
         {children}
         <Footer />
       </div>
