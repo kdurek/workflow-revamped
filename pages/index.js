a240bc08c8ae8ab92431ccc8bc3873b714fa9e3b
Krzysztof Durek <walendes@gmail.com>
2021-02-14 21:28:54 +0100

    refactor: Changed database collection names

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,17 @@ import Head from 'next/head';
 import firebaseClient from 'firebaseClient';
 import DefaultLayout from '@/layouts/DefaultLayout';
 import DashboardPage from '@/templates/DashboardPage';
+import {useAuth} from '@/context/AuthContext';
 
 const Index = () => {
+  const {user} = useAuth();
+
   const [tonersList, setTonersList] = useState([]);
 
   useEffect(() => {
     firebaseClient
       .firestore()
-      .collection('toners')
+      .collection(`toners${user.location}`)
       .where('amount', '<=', 1)
       .onSnapshot(snapshot => {
         const data = [];
