656db42818b9ef40f8ee7585552df287d4e26966
durashere <walendes@gmail.com>
2021-03-23 10:45:19 +0100

    style: change defaults

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,14 +7,13 @@ const Button = ({className, fullWidth, label, onClick, primary, square, type}) =
       onClick={onClick}
       type={type}
       className={classNames(
-        'flex items-center justify-center rounded-xl h-12 uppercase font-medium duration-300 transition-all focus:ring-2',
+        'flex items-center justify-center rounded-xl px-4 py-2 uppercase font-medium duration-300 transition-all focus:ring-2',
         className,
         {
           'bg-blue-400 text-white hover:bg-blue-500 hover:shadow-lg': primary,
           'bg-coolGray-100 hover:bg-coolGray-200 shadow-inner': !primary,
           'w-full': fullWidth,
-          'w-full md:w-48': !square && !fullWidth,
-          'w-12 h-12 p-3': square,
+          'w-10 h-10': square,
         }
       )}
     >
