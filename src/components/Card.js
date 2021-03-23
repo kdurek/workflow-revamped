af71519864cd95d825bf013a142c702a8b0cfd5f
durashere <walendes@gmail.com>
2021-03-23 10:02:34 +0100

    style: change padding

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import classNames from 'classnames';
 
 const Card = ({children, className}) => {
-  return <div className={classNames('p-4 rounded-xl bg-white shadow', className)}>{children}</div>;
+  return <div className={classNames('p-2 rounded-xl bg-white shadow', className)}>{children}</div>;
 };
 
 export default Card;
