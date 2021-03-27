c3f8fefa9f374d4054bcd13d9d557959ce20d828
Krzysztof <walendes@gmail.com>
2021-03-27 15:23:35 +0100

    refactor: rework component

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,71 +1,80 @@
-import {Transition} from '@headlessui/react';
-import {useRef, useState} from 'react';
+import {Listbox, Transition} from '@headlessui/react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
-import useClickOutside from '@/hooks/useClickOutside';
-
-const Select = ({label, optionLabel, options, value, onChange}) => {
-  const [open, setOpen] = useState(false);
-
-  const ref = useRef();
-  useClickOutside(ref, () => setOpen(false));
-
+const Select = ({label, onChange, optionLabel, options, value}) => {
   return (
-    <div ref={ref} className="relative w-full select-none">
-      <label className="mb-1 text-sm font-medium text-coolGray-500">{label}</label>
-      <button
-        onClick={() => setOpen(!open)}
-        className="relative w-full py-2 pl-4 pr-8 text-left bg-white rounded-md shadow cursor-default ring-1 ring-opacity-50 ring-coolGray-300 focus:ring-blue-500"
-      >
-        {value ? (
-          <span className="flex items-center">
-            <span className="truncate">{optionLabel ? value[optionLabel] : value}</span>
-          </span>
-        ) : (
-          <span className="flex items-center">
-            <span className="truncate">Click to select...</span>
-          </span>
-        )}
+    <Listbox as="div" className="w-full space-y-1" value={value} onChange={onChange}>
+      {({open}) => (
+        <>
+          <Listbox.Label className="block text-sm font-medium text-coolGray-500">
+            {label}
+          </Listbox.Label>
+          <div className="relative">
+            <Listbox.Button className="relative w-full py-2 pl-4 pr-10 text-left transition bg-white rounded-md shadow cursor-default ring-1 ring-opacity-50 ring-coolGray-300 focus:ring-blue-300">
+              <span className="block truncate">
+                {value ? (optionLabel ? value[optionLabel] : value) : 'Click to select...'}
+              </span>
+              <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none text-coolGray-400 material-icons">
+                unfold_more
+              </span>
+            </Listbox.Button>
 
-        <span className="absolute inset-y-0 right-0 flex items-center pr-2 text-coolGray-400 material-icons">
-          unfold_more
-        </span>
-      </button>
-      <Transition
-        show={open}
-        leave="transition-all duration-100 ease-in"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0"
-        className="absolute z-40 w-full mt-1 bg-white rounded-md shadow-lg"
-      >
-        <ul className="py-1 overflow-auto rounded-md scrollbar scrollbar-thumb-coolGray-300 scrollbar-thumb-rounded-md scrollbar-thin max-h-56 ring-1 ring-coolGray-300 ring-opacity-50 focus:outline-none sm:text-sm">
-          {options?.map((option, i) => (
-            <li key={optionLabel ? option._id : i}>
-              <button
-                onClick={() => {
-                  setOpen(false);
-                  onChange(option);
-                }}
-                className={classNames(
-                  'relative flex items-center w-full py-2 pl-4 pr-10 cursor-default group hover:bg-blue-500',
-                  {'font-semibold': option === value}
-                )}
+            <Transition
+              show={open}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+              className="absolute z-40 w-full mt-1 bg-white rounded-md shadow-lg ring-1 ring-opacity-50 ring-coolGray-300"
+            >
+              <Listbox.Options
+                static
+                className="w-full py-1 overflow-auto rounded-md max-h-60 focus:outline-none"
               >
-                <span className="truncate group-hover:text-white">
-                  {optionLabel ? option[optionLabel] : option}
-                </span>
-                {option === value && (
-                  <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-blue-500 group-hover:text-white material-icons">
-                    check
-                  </span>
-                )}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </Transition>
-    </div>
+                {options?.map(option => (
+                  <Listbox.Option
+                    className="focus:outline-none"
+                    key={optionLabel ? option._id : option}
+                    value={option}
+                  >
+                    {({selected, active}) => (
+                      <div
+                        className={classNames(
+                          'cursor-default select-none relative py-2 pl-10 pr-4',
+                          {'text-white bg-blue-600': active}
+                        )}
+                      >
+                        <span
+                          className={classNames('block truncate', {
+                            'font-semibold': selected,
+                            'font-normal': !selected,
+                          })}
+                        >
+                          {optionLabel ? option[optionLabel] : option}
+                        </span>
+                        {selected && (
+                          <span
+                            className={classNames(
+                              'absolute inset-y-0 left-0 flex items-center pl-2 material-icons',
+                              {
+                                'text-white': active,
+                                'text-blue-600': !active,
+                              }
+                            )}
+                          >
+                            check
+                          </span>
+                        )}
+                      </div>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </div>
+        </>
+      )}
+    </Listbox>
   );
 };
 
