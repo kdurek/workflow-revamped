acc213388a977368814f0d17b5aaf0f46fe71d09
durashere <walendes@gmail.com>
2021-04-02 10:42:12 +0200

    style: adjust colors

--- src/modules/printers/utils/getTonerColor.js
+++ src/modules/printers/utils/getTonerColor.js
@@ -1,13 +1,13 @@
 const getTonerColor = color => {
   switch (color) {
     case 'Black':
-      return 'bg-gray-300';
+      return 'bg-gray-200 text-gray-900';
     case 'Cyan':
-      return 'bg-cyan-200';
+      return 'bg-blue-200 text-blue-900';
     case 'Magenta':
-      return 'bg-fuchsia-200';
+      return 'bg-purple-200 text-purple-900';
     case 'Yellow':
-      return 'bg-yellow-200';
+      return 'bg-yellow-200 text-yellow-900';
 
     default:
       return 'bg-transparent';
