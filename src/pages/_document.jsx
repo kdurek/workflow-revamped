de0ef607a619e932980024648582cffc6a45cecc
Krzysztof <walendes@gmail.com>
2021-05-13 16:05:46 +0200

    fix: material-icons not scalling

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -5,16 +5,17 @@ class CustomDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic"
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
             rel="stylesheet"
           />
           <link
-            href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp"
             rel="stylesheet"
           />
         </Head>
-        <body className="scrollbar scrollbar-thumb-gray-300 scrollbar-thumb-rounded-xl bg-gray-100 text-gray-900">
+        <body className="text-gray-900 bg-gray-100 scrollbar scrollbar-thumb-gray-300 scrollbar-thumb-rounded-xl">
           <Main />
           <NextScript />
         </body>
