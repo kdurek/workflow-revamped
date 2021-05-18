4c44d7e4e358cf12bd9d0703cb097f8e921a64ff
durashere <walendes@gmail.com>
2021-05-18 15:40:22 +0200

    refactor: cleanup code

--- src/modules/reactQuery/mutations/useUpdateToner.js
+++ src/modules/reactQuery/mutations/useUpdateToner.js
@@ -2,7 +2,6 @@ import {useMutation, useQueryClient} from 'react-query';
 import axios from 'axios';
 
 const updateToner = async updatedToner => {
-  console.log(`file: useUpdateToner.js > line 5 > updatedToner`, updatedToner);
   const {data} = await axios.patch(
     `${process.env.NEXT_PUBLIC_BACKEND_URL}/toners/${updatedToner._id}`,
     updatedToner
