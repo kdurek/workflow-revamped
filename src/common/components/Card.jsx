f445b9b3937b6167d846cd8b134a2fc0996b3ec2
durashere <walendes@gmail.com>
2021-05-19 14:29:48 +0200

    feat: add optional label

--- src/common/components/Card.jsx
+++ src/common/components/Card.jsx
@@ -1,17 +1,26 @@
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
-const Card = ({children, className}) => {
-  return <div className={classNames('p-4 rounded-xl bg-white shadow', className)}>{children}</div>;
+const Card = ({children, className, label}) => {
+  return (
+    <div className={classNames('p-4 rounded-xl bg-white shadow', className)}>
+      <>
+        <span className="text-xl font-medium select-none">{label}</span>
+        {children}
+      </>
+    </div>
+  );
 };
 
 Card.propTypes = {
   children: PropTypes.oneOfType([PropTypes.object, PropTypes.arrayOf(PropTypes.object)]).isRequired,
   className: PropTypes.oneOfType([PropTypes.string, PropTypes.objectOf(PropTypes.object)]),
+  label: PropTypes.string,
 };
 
 Card.defaultProps = {
   className: undefined,
+  label: undefined,
 };
 
 export default Card;
